05280721008f05e2a47ca19d8a4cda1411375b98
Alex <alex.martinez2017@alu.uct.cl>
2021-09-29 18:12:40 -0300

    Creación de Directorio de la Aplicación
    Mitad de la creación de las rutas, ademas del directorio completo de la aplicación.
    
    Lo subí yo debido a  inclemencias climaticas de Nicolas
    
    Co-Authored-By: Nicolas Calquin <74797623+ncalquin@users.noreply.github.com>

---
+++ Proyecto/Proyecto/server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/add')
+def add():
+    return 'Agregar documento'
+
+@app.route('/delete/<document>/<id>/')
+def delete(document, id):
+    return 'Borrar documento : {}, {}'.format(document, id)
+
+@app.route('/export/<document>/<id>/')
+def export(document, id):
+    return 'Exportar docuemento : {}, {}'.format(document, id)
+
+@app.route('/edit/<document>/<id>/')
+def edit(document, id):
+    return render_template('edit.html')
+
+app.run(debug = True)
+
