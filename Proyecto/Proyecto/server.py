b9e477302d90c55e6eb270f52bae019e1b791e35
RaulRodriguezSep <74796616+RaulRodriguezSep@users.noreply.github.com>
2021-09-30 21:19:57 -0300

    Update server.py

--- Proyecto/Proyecto/server.py
+++ Proyecto/Proyecto/server.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+from flask_pymongo import PyMongo
+from werkzeug.wrappers import response
 
 app = Flask(__name__)
+app.config['MONGO_URI']='mongodb://localhost:27017/pruebassss'
+mongo=PyMongo(app)
 
 @app.route('/')
 def home():
@@ -22,6 +26,26 @@ def export(document, id):
 @app.route('/edit/<document>/<id>/')
 def edit(document, id):
     return render_template('edit.html')
+
+@app.route('/probar', methods=['POST'])
+def create_doc():
+    nombreDoc = request.json['nombreDoc']
+    autor = request.json['autor']
+    fecha = request.json['fecha']
+
+    if nombreDoc and autor and fecha:
+        id =  mongo.db.documentos.insert(
+       {'nombreDoc':nombreDoc, 'autor':autor, 'fecha':fecha}
+       )
+        response ={
+            'id': str(id),
+            'nombreDoc': nombreDoc,
+            'autor': autor,
+            'fecha': fecha
+            }   
+        return response
+    else:{'massage':'recived'}
+    return{'massage':'recived'}
 
 app.run(debug = True)
 
