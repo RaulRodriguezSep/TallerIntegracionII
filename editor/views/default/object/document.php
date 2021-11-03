07efcb106498d501e2f10f4e7401082f544001f6
Nicolas Calquin <74797623+ncalquin@users.noreply.github.com>
2021-11-03 20:20:50 -0300

    Update document.php

--- editor/views/default/object/document.php
+++ editor/views/default/object/document.php
@@ -1,4 +1,16 @@
 <?php
+$full = elgg_extract('full_view', $vars, FALSE);
 
-echo elgg_view('output/longtext', array('value' => $vars['entity']->description));
-echo elgg_view('output/tags', array('tags' => $vars['entity']->tags));
+if ($full) {
+    echo elgg_view('output/longtext', array('value' => $vars['entity']->description));
+    echo elgg_view('output/tags', array('tags' => $vars['entity']->tags));
+
+} else {
+    echo elgg_view_title(
+        elgg_view('output/url', array(
+            'href' => $vars['entity']->getURL(),
+            'text' => $vars['entity']->getDisplayName(),
+            'is_trusted' => true,
+    )));
+    echo elgg_view('output/tags', array('tags' => $vars['entity']->tags));
+}
