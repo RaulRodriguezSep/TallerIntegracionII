857eb793ef5c092b58a5783124cffc9d9a0e4d9d
Nicolas Calquin <74797623+ncalquin@users.noreply.github.com>
2021-12-01 23:49:47 -0300

    Update save.php

--- editor/actions/document/save.php
+++ editor/actions/document/save.php
@@ -1,7 +1,6 @@
 <?php
 
 $title = elgg_get_title_input('title');
-$body = get_input('body');
 
 $document = new ElggObject();
 $document->subtype = 'document';
@@ -9,9 +8,9 @@ $document->access_id = ACCESS_PUBLIC;
 $document->owner_guid = elgg_get_logged_in_user_guid();
 
 $document->title = $title;
-$document->description = $body;
 
 if ($document->save()) {
+   echo exec("python save.py $title");
    return elgg_ok_response('', "Documento creado exitosamente", '/editor/all');
 } else {
    return elgg_error_response("No se pudo crear el documento");
